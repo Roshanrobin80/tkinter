b34bfc36031c862fb318f80f105706a04fe0a444
Roshanrobin80 <roshanrobin220303@gmail.com>
2024-10-17 22:00:49 +0530

    Update login.py

--- login.py
+++ login.py
@@ -5,10 +5,29 @@ win.title=('Login')
 win.configure(bg='purple')
 win.minsize(400,400)
 win.maxsize(500,500)
-def save():
-    
 
-l1=tkinter.Label(win,text='LOGIN',bg='purple',fg='white')
+
+def save():
+    win1=tkinter.Tk()
+    win1.title('Register')
+    win1.configure(bg='cyan')
+    win1.minsize(400,400)
+    win1.maxsize(500,500)
+    l4=tkinter.Label(win1,text='REGISTER',bg='cyan',fg='black')
+    l4.pack()
+    l5=tkinter.Label(win1,text='User Name')
+    l5.place(x=75,y=50)
+    l6=tkinter.Label(win1,text='Password')
+    l6.place(x=75,y=100)
+    e3=tkinter.Entry(win1)
+    e3.place(x=170,y=50)
+    e4=tkinter.Entry(win1)
+    e4.place(x=170,y=100)
+    btn2=tkinter.Button(win1,text='submit',bg='gray',activebackground='green',activeforeground='white',padx=3,pady=3)
+    btn2.place(x=200,y=200)
+    win1.mainloop()
+
+l1=tkinter.Label(win,text='LOGIN',bg='purple',fg='black')
 l1.pack()
 
 l2=tkinter.Label(win,text='User Name')
@@ -17,8 +36,7 @@ l2.place(x=75,y=50)
 l3=tkinter.Label(win,text='Password')
 l3.place(x=75,y=100)
 
-# l4=tkinter.Label(win,text='Login')
-# l4.place(x=200,y=170)
+
 
 e1=tkinter.Entry(win)
 e1.place(x=170,y=50)
@@ -26,8 +44,11 @@ e1.place(x=170,y=50)
 e2=tkinter.Entry(win)
 e2.place(x=170,y=100)
 
-btn1=tkinter.Button(win,text='Login',bg='gray',activebackground='black',activeforeground='white',padx=10,pady=8,command=save)
-btn1.place(x=150,y=80)
+
+btn=tkinter.Button(win,text='submit',bg='gray',activebackground='black',activeforeground='white',padx=3,pady=3,)
+btn.place(x=200,y=200)
+btn1=tkinter.Button(win,text='register',bg='gray',activebackground='green',activeforeground='black',padx=3,pady=3,command=save)
+btn1.place(x=50,y=250)
 
 
 win.mainloop()
